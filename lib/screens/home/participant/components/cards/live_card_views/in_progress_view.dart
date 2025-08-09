83c3343e3c7a51eb45c7bffbae4832b2ceb0e30c
Koray Öztürkler <kerimkoray.ozturkler1@gmail.com>
2025-08-09 15:56:05 +0300

    Enhance LiveCardInProgressView with dynamic flavor text and improved countdown handling

--- lib/screens/home/participant/components/cards/live_card_views/in_progress_view.dart
+++ lib/screens/home/participant/components/cards/live_card_views/in_progress_view.dart
@@ -1,11 +1,159 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hackncsu_today/features/streams/event_state_stream.dart';
-import 'package:hackncsu_today/models/event/event_state.dart';
 import 'package:hackncsu_today/models/extensions/duration.dart';
 
-class LiveCardInProgressView extends ConsumerWidget {
+class LiveCardInProgressView extends ConsumerStatefulWidget {
   const LiveCardInProgressView({super.key});
+
+  @override
+  ConsumerState<LiveCardInProgressView> createState() =>
+      _LiveCardInProgressViewState();
+}
+
+class _LiveCardInProgressViewState
+    extends ConsumerState<LiveCardInProgressView> {
+  static const Duration _firstHourEnd = Duration(hours: 23);
+  static const Duration _halfwayPointStart = Duration(hours: 12);
+  static const Duration _halfwayPointEnd = Duration(hours: 10);
+  static const Duration _finalStretchStart = Duration(hours: 1);
+  static const Duration _lastCallStart = Duration(minutes: 30);
+
+  static const List<String> _genericFlavorTitles = [
+    'Hack_NCState is in progress!',
+  ];
+  static const List<String> _genericFlavorText = [
+    'Keep coding!',
+    'Lock in.',
+    'I\'ll think of something later.',
+  ];
+
+  static const List<String> _firstHourFlavorTitles = [
+    'Hack_NCState has begun!',
+  ];
+  static const List<String> _firstHourFlavorText = [
+    'Can your ideas change the world?',
+    'Let the coding marathon commence!',
+    "Let's build something amazing together!",
+    'Unleash your creativity and innovation!',
+    'The future is yours to create!',
+    'Start brainstorming and coding your ideas!',
+  ];
+
+  static const List<String> _halfwayPointFlavorTitles = [
+    'Halfway done!',
+    'You\'re halfway through!',
+  ];
+  static const List<String> _halfwayPointFlavorText = [
+    'Keep at it!',
+    'Keep pushing forward!',
+    "Let's finish strong!",
+    'Keep the momentum going!',
+    'Let\'s keep the energy up!',
+    'Keep the ideas flowing!',
+  ];
+
+  static const List<String> _finalStretchFlavorTitles = [
+    'Final stretch!',
+    'Last hour!',
+    'Final hour!',
+  ];
+  static const List<String> _finalStretchFlavorText = [
+    'Finish strong!',
+    'Let\'s wrap this up!',
+    'Don\'t forget to submit your project!',
+    'Make sure your project is ready for submission!',
+    'Double-check your project and submit it!',
+    'Make sure your project is ready for the judges!',
+  ];
+
+  static const List<String> _lastCallFlavorTitles = [
+    'Last call!',
+    'Final call!',
+    'Last chance!',
+    'Final chance!',
+  ];
+  static const List<String> _lastCallFlavorText = [
+    'Make sure your project is submitted!',
+    'Submit your project now!',
+    'Don\'t miss your chance to submit!',
+    'Ensure your project is ready for submission!',
+  ];
+
+  late Timer _timer;
+  int _randomSeed = DateTime.now().millisecondsSinceEpoch;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _timer = Timer.periodic(const Duration(minutes: 30), (timer) {
+      setState(() {
+        _randomSeed = DateTime.now().millisecondsSinceEpoch;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer.cancel();
+  }
+
+  /// Builds the countdown view based on the current countdown duration.
+  Widget _randomFlavorTextBuilder(BuildContext context, Duration countdown) {
+    final random = Random(_randomSeed);
+
+    if (countdown > _firstHourEnd) {
+      return _flavorTextBuilder(
+        context,
+        _firstHourFlavorTitles[random.nextInt(_firstHourFlavorTitles.length)],
+        _firstHourFlavorText[random.nextInt(_firstHourFlavorText.length)],
+      );
+    } else if (countdown > _halfwayPointStart) {
+      return _flavorTextBuilder(
+        context,
+        _genericFlavorTitles[random.nextInt(_genericFlavorTitles.length)],
+        _genericFlavorText[random.nextInt(_genericFlavorText.length)],
+      );
+    } else if (countdown > _halfwayPointEnd) {
+      return _flavorTextBuilder(
+        context,
+        _halfwayPointFlavorTitles[random.nextInt(
+          _halfwayPointFlavorTitles.length,
+        )],
+        _halfwayPointFlavorText[random.nextInt(_halfwayPointFlavorText.length)],
+      );
+    } else if (countdown > _finalStretchStart) {
+      return _flavorTextBuilder(
+        context,
+        _genericFlavorTitles[random.nextInt(_genericFlavorTitles.length)],
+        _genericFlavorText[random.nextInt(_genericFlavorText.length)],
+      );
+    } else if (countdown > _lastCallStart) {
+      return _flavorTextBuilder(
+        context,
+        _finalStretchFlavorTitles[random.nextInt(
+          _finalStretchFlavorTitles.length,
+        )],
+        _finalStretchFlavorText[random.nextInt(_finalStretchFlavorText.length)],
+      );
+    } else {
+      return _flavorTextBuilder(
+        context,
+        _lastCallFlavorTitles[random.nextInt(_lastCallFlavorTitles.length)],
+        _lastCallFlavorText[random.nextInt(_lastCallFlavorText.length)],
+      );
+    }
+  }
+
+  /// Builds a view with a random title and text from the provided lists.
+  Widget _flavorTextBuilder(BuildContext context, String title, String text) {
+    return Text('$title $text', style: Theme.of(context).textTheme.titleLarge);
+  }
 
   Widget _countdownBuilder(BuildContext context, Duration countdown) {
     return Column(
@@ -17,10 +165,7 @@ class LiveCardInProgressView extends ConsumerWidget {
             context,
           ).textTheme.displayMedium?.copyWith(fontWeight: FontWeight.bold),
         ),
-        Text(
-          'Hack_NCState is in progress!',
-          style: Theme.of(context).textTheme.titleLarge,
-        ),
+        _randomFlavorTextBuilder(context, countdown),
       ],
     );
   }
@@ -30,7 +175,7 @@ class LiveCardInProgressView extends ConsumerWidget {
   }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final countdown = ref.watch(inProgressCountdownStreamProvider);
 
     return countdown.when(
