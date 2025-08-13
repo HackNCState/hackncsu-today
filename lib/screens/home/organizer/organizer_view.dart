6d4fd6f9b870277250628b3b29f3a46a237ab23b
Koray Öztürkler <kerimkoray.ozturkler1@gmail.com>
2025-08-13 15:17:49 +0300

    Refactor OrganizerView to utilize TaskService

--- lib/screens/home/organizer/organizer_view.dart
+++ lib/screens/home/organizer/organizer_view.dart
@@ -11,129 +11,12 @@ import 'package:hackncsu_today/screens/home/organizer/models/task.dart';
 import 'package:hackncsu_today/screens/home/participant/components/cards/live_card.dart';
 import 'package:hackncsu_today/screens/home/participant/components/cards/resource_card.dart';
 import 'package:hackncsu_today/services/firebase/firestore_service.dart';
+import 'package:hackncsu_today/services/organizer/task_service.dart';
 
 class OrganizerView extends ConsumerWidget {
   const OrganizerView({super.key});
 
-  List<Task> get _quickTasks => [
-    Task(
-      title: 'Initialize Event',
-      content:
-          'Initializes hackathon event state and data in database, and adds sample data.\n'
-          'This creates structure for hackathon state and supplementary data (links, resources, etc.)\n\n'
-          'Should be executed once at the start of the event, before any participants join.\n'
-          'Afterwards, edit the data to set the discord server invite etc. and make sure to set hidden to false so it shows up for participants.',
-      onExecute: (ref, _) async {
-        final firestoreService = ref.read(firebaseFirestoreServiceProvider);
-        await firestoreService.initializeEvent();
-      },
-    ),
-    Task(
-      title: 'Post announcement',
-      content:
-          'Posts an announcement to the event state.\n'
-          'This is useful for quick announcements or updates during the event.\n'
-          'It will appear under the countdown timer on the Live Card with format "Announcement: <announcement>".\n'
-          'Please remember to remove it when it is no longer relevant.',
-      parameters: (_) => [TaskParameter.string('Content')],
-      onExecute: (ref, parameters) async {
-        final firestoreService = ref.read(firebaseFirestoreServiceProvider);
-        final announcement = parameters[0] as StringTaskParameter;
-
-        final eventState = ref.read(eventStateStreamProvider).valueOrNull;
-        if (eventState == null) {
-          throw TaskExecutionException('Event state is not initialized.');
-        }
-
-        final updatedEventState = eventState.copyWith(
-          announcement: announcement.value.trim(),
-        );
-        await firestoreService.updateEventState(updatedEventState);
-      },
-    ),
-    Task(
-      title: 'Remove announcement',
-      content:
-          'Removes the announcement from the event state.\n'
-          'This is useful when the announcement is no longer relevant.',
-      onExecute: (ref, _) async {
-        final firestoreService = ref.read(firebaseFirestoreServiceProvider);
-        final eventState = ref.read(eventStateStreamProvider).valueOrNull;
-        if (eventState == null) {
-          throw TaskExecutionException('Event state is not initialized.');
-        }
-
-        final updatedEventState = eventState.copyWith(announcement: null);
-        await firestoreService.updateEventState(updatedEventState);
-      },
-    ),
-  ];
-
-  List<Task> get _eventStateManagementTasks => [
-    Task(
-      title: 'Set State to Initial State',
-      content:
-          'Changes the event state to Initial.\n'
-          'Pre-event state, before opening ceremony.',
-      onExecute: (ref, _) async {
-        final firestoreService = ref.read(firebaseFirestoreServiceProvider);
-        await firestoreService.updateEventState(EventState.initial());
-      },
-    ),
-    Task(
-      title: 'Set State to Standby',
-      content:
-          'Changes the event state to Standby.\n'
-          'This should be executed between states (between in progress and judging, for example).',
-      onExecute: (ref, _) async {
-        final firestoreService = ref.read(firebaseFirestoreServiceProvider);
-        await firestoreService.updateEventState(EventState.standby());
-      },
-    ),
-    Task(
-      title: 'Set State to Opening Ceremony',
-      content:
-          'Changes the event state to Opening Ceremony.\n'
-          'This should be executed when the opening ceremony starts.',
-      onExecute: (ref, _) async {
-        final firestoreService = ref.read(firebaseFirestoreServiceProvider);
-        await firestoreService.updateEventState(EventState.openingCeremony());
-      },
-    ),
-    Task(
-      title: 'Set State to In Progress',
-      content:
-          'Changes the event state to In Progress.\n'
-          'This should be executed when the hackathon starts.',
-      parameters:
-          (_) => [
-            TaskParameter.dateTime(
-              'Hackathon End Time\n(default: 24h from now)',
-              DateTime.now().add(const Duration(hours: 24)),
-            ),
-          ],
-      onExecute: (ref, parameters) async {
-        final firestoreService = ref.read(firebaseFirestoreServiceProvider);
-
-        final startTime = DateTime.now();
-        final endTime = parameters[0] as DateTimeTaskParameter;
-
-        await firestoreService.updateEventState(
-          EventState.inProgress(startTime: startTime, endTime: endTime.value),
-        );
-      },
-    ),
-    Task(
-      title: 'Set State to Closing Ceremony',
-      content:
-          'Changes the event state to Closing Ceremony.\n'
-          'This should be executed when the closing ceremony starts.',
-      onExecute: (ref, _) async {
-        final firestoreService = ref.read(firebaseFirestoreServiceProvider);
-        await firestoreService.updateEventState(EventState.closingCeremony());
-      },
-    ),
-  ];
+  
 
   List<Widget> _cardsBuilder(BuildContext context) => [
     LiveCard(),
@@ -184,6 +67,8 @@ class OrganizerView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final taskService = ref.watch(taskServiceProvider);
+
     return Center(
       child: SingleChildScrollView(
         child: Column(
@@ -209,7 +94,7 @@ class OrganizerView extends ConsumerWidget {
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 8),
-            _taskWrapBuilder(context, _quickTasks),
+            _taskWrapBuilder(context, taskService.quickTasks),
             const SizedBox(height: 16),
             Divider(),
             const SizedBox(height: 16),
@@ -222,7 +107,7 @@ class OrganizerView extends ConsumerWidget {
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 8),
-            _taskWrapBuilder(context, _eventStateManagementTasks),
+            _taskWrapBuilder(context, taskService.eventStateManagementTasks),
             const SizedBox(height: 16),
             Divider(),
             const SizedBox(height: 16),
@@ -249,8 +134,4 @@ class OrganizerView extends ConsumerWidget {
       ),
     );
   }
-}
-
-class TaskExecutionException extends AppException {
-  TaskExecutionException(super.message);
 }
---
+++ lib/services/organizer/task_service.dart
@@ -0,0 +1,141 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hackncsu_today/exception.dart';
+import 'package:hackncsu_today/features/streams/event_state_stream.dart';
+import 'package:hackncsu_today/models/event/event_state.dart';
+import 'package:hackncsu_today/screens/home/organizer/models/task.dart';
+import 'package:hackncsu_today/services/firebase/firestore_service.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'task_service.g.dart';
+
+/// Manages tasks that organizers can run
+@riverpod
+TaskService taskService(Ref ref) {
+  return TaskService();
+}
+
+class TaskService {
+  List<Task> get quickTasks => [
+    Task(
+      title: 'Initialize Event',
+      content:
+          'Initializes hackathon event state and data in database, and adds sample data.\n'
+          'This creates structure for hackathon state and supplementary data (links, resources, etc.)\n\n'
+          'Should be executed once at the start of the event, before any participants join.\n'
+          'Afterwards, edit the data to set the discord server invite etc. and make sure to set hidden to false so it shows up for participants.',
+      onExecute: (ref, _) async {
+        final firestoreService = ref.read(firebaseFirestoreServiceProvider);
+        await firestoreService.initializeEvent();
+      },
+    ),
+    Task(
+      title: 'Post announcement',
+      content:
+          'Posts an announcement to the event state.\n'
+          'This is useful for quick announcements or updates during the event.\n'
+          'It will appear under the countdown timer on the Live Card with format "Announcement: <announcement>".\n'
+          'Please remember to remove it when it is no longer relevant.',
+      parameters: (_) => [TaskParameter.string('Content')],
+      onExecute: (ref, parameters) async {
+        final firestoreService = ref.read(firebaseFirestoreServiceProvider);
+        final announcement = parameters[0] as StringTaskParameter;
+
+        final eventState = ref.read(eventStateStreamProvider).valueOrNull;
+        if (eventState == null) {
+          throw TaskExecutionException('Event state is not initialized.');
+        }
+
+        final updatedEventState = eventState.copyWith(
+          announcement: announcement.value.trim(),
+        );
+        await firestoreService.updateEventState(updatedEventState);
+      },
+    ),
+    Task(
+      title: 'Remove announcement',
+      content:
+          'Removes the announcement from the event state.\n'
+          'This is useful when the announcement is no longer relevant.',
+      onExecute: (ref, _) async {
+        final firestoreService = ref.read(firebaseFirestoreServiceProvider);
+        final eventState = ref.read(eventStateStreamProvider).valueOrNull;
+        if (eventState == null) {
+          throw TaskExecutionException('Event state is not initialized.');
+        }
+
+        final updatedEventState = eventState.copyWith(announcement: null);
+        await firestoreService.updateEventState(updatedEventState);
+      },
+    ),
+  ];
+
+  List<Task> get eventStateManagementTasks => [
+    Task(
+      title: 'Set State to Initial State',
+      content:
+          'Changes the event state to Initial.\n'
+          'Pre-event state, before opening ceremony.',
+      onExecute: (ref, _) async {
+        final firestoreService = ref.read(firebaseFirestoreServiceProvider);
+        await firestoreService.updateEventState(EventState.initial());
+      },
+    ),
+    Task(
+      title: 'Set State to Standby',
+      content:
+          'Changes the event state to Standby.\n'
+          'This should be executed between states (between in progress and judging, for example).',
+      onExecute: (ref, _) async {
+        final firestoreService = ref.read(firebaseFirestoreServiceProvider);
+        await firestoreService.updateEventState(EventState.standby());
+      },
+    ),
+    Task(
+      title: 'Set State to Opening Ceremony',
+      content:
+          'Changes the event state to Opening Ceremony.\n'
+          'This should be executed when the opening ceremony starts.',
+      onExecute: (ref, _) async {
+        final firestoreService = ref.read(firebaseFirestoreServiceProvider);
+        await firestoreService.updateEventState(EventState.openingCeremony());
+      },
+    ),
+    Task(
+      title: 'Set State to In Progress',
+      content:
+          'Changes the event state to In Progress.\n'
+          'This should be executed when the hackathon starts.',
+      parameters:
+          (_) => [
+            TaskParameter.dateTime(
+              'Hackathon End Time\n(default: 24h from now)',
+              DateTime.now().add(const Duration(hours: 24)),
+            ),
+          ],
+      onExecute: (ref, parameters) async {
+        final firestoreService = ref.read(firebaseFirestoreServiceProvider);
+
+        final startTime = DateTime.now();
+        final endTime = parameters[0] as DateTimeTaskParameter;
+
+        await firestoreService.updateEventState(
+          EventState.inProgress(startTime: startTime, endTime: endTime.value),
+        );
+      },
+    ),
+    Task(
+      title: 'Set State to Closing Ceremony',
+      content:
+          'Changes the event state to Closing Ceremony.\n'
+          'This should be executed when the closing ceremony starts.',
+      onExecute: (ref, _) async {
+        final firestoreService = ref.read(firebaseFirestoreServiceProvider);
+        await firestoreService.updateEventState(EventState.closingCeremony());
+      },
+    ),
+  ];
+}
+
+class TaskExecutionException extends AppException {
+  TaskExecutionException(super.message);
+}
