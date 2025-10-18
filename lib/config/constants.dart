229d7c4792f082dc51e184385ce4c91a75e42b40
Koray Özturkler <kerimkoray.ozturkler1@gmail.com>
2025-10-18 10:20:06 -0400

    Remove all references to Flutter

--- lib/config/constants.dart
+++
@@ -1,4 +0,0 @@
-const bool kFirebaseUseEmulator = true;
-const bool kEmbedDiscord = true;
-const String kDiscordServerId = '1329464908986716265';
-const String kDiscordChannelId = '1329464909641158754';
