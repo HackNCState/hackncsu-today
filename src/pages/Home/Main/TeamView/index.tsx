c50de473ace72dc6085f0f19c4b64ff39294ea1a
Koray Özturkler <kerimkoray.ozturkler1@gmail.com>
2026-01-16 19:15:37 -0500

    UI cleanup

--- src/pages/Home/Main/TeamView/index.tsx
+++ src/pages/Home/Main/TeamView/index.tsx
@@ -14,9 +14,13 @@ import { teamAtom } from "@/atoms/team";
 import { useAtomValue } from "jotai";
 import UnapprovedView from "./UnapprovedView";
 import ApprovedView from "./ApprovedView";
+import { isOrganizerAtom } from "@/atoms/user";
 
 export default function TeamView() {
 	const team = useAtomValue(teamAtom);
+	const isOrganizer = useAtomValue(isOrganizerAtom);
+
+	if (isOrganizer) return null;
 
 	function buildView() {
 		if (team) {
@@ -33,7 +37,11 @@ export default function TeamView() {
 	return (
 		<FeedItem
 			title="Your Team"
-			description={!team ? "You'll need a team to help you during Hack_NCState! Once you have one, please fill out the team formation form below. Only the team leader needs to complete this form." : null}
+			description={
+				!team
+					? "You'll need a team to help you during Hack_NCState! Once you have one, please fill out the team formation form below. Only the team leader needs to complete this form."
+					: null
+			}
 		>
 			{buildView()}
 		</FeedItem>
